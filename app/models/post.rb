b025ad3fd206684aae092ebb3ec9185bf323f644
ttovarischh <polinasot@gmail.com>
2021-10-21 18:59:14 +0300

    adminka

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 		validates :name, :presence => true
 		validates :title, :presence => true,
-											:length => { :minimum => 5 }
+											:length => { :minimum => 0 }
 
     mount_uploader :image, ImageUploader
     has_many :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_132426) do
+ActiveRecord::Schema.define(version: 2021_10_21_144848) do
+
+  create_table "commenticons", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
