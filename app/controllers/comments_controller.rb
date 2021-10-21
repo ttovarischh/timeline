d7b73d9c70a1aaede85ce19267e9af5b56c61736
Maria <kauztmaria@gmail.com>
2021-10-21 20:20:38 +0300

    comment fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+		@comment = @post.comments.create(params[:comment].permit(:commenter, :body, :date, :position))
 		redirect_to post_path(@post)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_144848) do
-
-  create_table "commenticons", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_10_21_132426) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
